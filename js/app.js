8eabef3f5d1100e54268ac462b892995c4fc2976
ottino <maxi.ottino@gmail.com>
2020-08-29 20:38:18 -0300

    Casos por pais y por provincia. Se agrega data.csv de la provincia

--- js/app.js
+++ js/app.js
@@ -8,104 +8,142 @@ if (navigator.serviceWorker) {
             });
 };
 
-const urlCovid_hoy = `https://disease.sh/v3/covid-19/countries/ARG?yesterday=true&twoDaysAgo=true&strict=true&allowNull=true`;
-const divTablero = document.querySelector('#tablero');
+const urlCovid_hoy            = `https://disease.sh/v3/covid-19/countries/ARG?yesterday=true&twoDaysAgo=true&strict=true&allowNull=true`;
+const divTableroPais          = document.querySelector('#tableroPais');
+const divTableroProvinciaHoy  = document.querySelector('#provincia_hoy');
+const divTableroProvinciaAyer = document.querySelector('#provincia_ayer');
 
-//divTablero.innerHTML = 
-
-// `
-// <div class="card">
-//  <div class="card-body">
-//   <h5 class="card-title">Card title</h5>
-//   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//  </div>
-//  <div class="card-footer">
-//   <small class="text-muted">Last updated 3 mins ago</small>
-//  </div>
-// </div>
-// `;
-
-
-
-
-// Total de casos ahora
+// Tablero -> Pais
 fetch( urlCovid_hoy )
         .then( resp => resp.json() )
         .then( resp => {
 
-                const { todayCases , todayDeaths , active , cases , deaths , recovered , tests , updated } = resp;
-                let fecha = new Date( updated );
+                const { todayCases , 
+                        todayDeaths , 
+                        active , 
+                        cases , 
+                        deaths , 
+                        recovered , 
+                        tests , 
+                        updated ,
+                        casesPerOneMillion ,
+                        deathsPerOneMillion ,
+                        testsPerOneMillion } = resp;
 
-                console.log( { todayCases , active , cases , deaths , recovered , tests , updated } );
-
-                console.log( fecha );
-                console.table( resp );
-
-                divTablero.innerHTML = 
+                divTableroPais.innerHTML = 
                 `
                 <div class="card">
-                 <div class="card-body">
-                  <h5 class="card-title">Datos actualizados a la fecha ${ fecha.getDate() }/${ fecha.getMonth()+1 } </h5>
+                 <div class="card-body carta_ultrep">
+                  <h5 class="card-title">Ultimo reporte</h5>
                   <p class="card-text">
-                        Contagiados: ${ todayCases } <br>
-                        Fallecidos: ${ todayDeaths }
+                        Contagiados: ${ todayCases.toLocaleString() } <br>
+                        Fallecidos: ${ todayDeaths.toLocaleString() } <br>
+                        Test. Acum.: ${ tests.toLocaleString() } <br>
                   </p>
-                 </div>
-                 <div class="card-footer">
-                  <small class="text-muted">Last updated 3 mins ago</small>
                  </div>
                 </div>
 
-                <div class="card">
+                <div class="card carta_acumulado">
                  <div class="card-body">
-                  <h5 class="card-title">Acumulado </h5>
+                  <h5 class="card-title">Acumulado</h5>
                   <p class="card-text">
-                        Contagiados: ${ cases } <br>
-                        Fallecidos: ${ deaths } <br>
-                        Recuperados: ${ recovered }
+                        Contagiados: ${ cases.toLocaleString() } <br>
+                        Fallecidos: ${ deaths.toLocaleString() } <br>
+                        Recuperados: ${ recovered.toLocaleString() }
                   </p>
                  </div>
-                 <div class="card-footer">
-                  <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+
+                <div class="card carta_millon">
+                 <div class="card-body">
+                  <h5 class="card-title">Por millon</h5>
+                  <p class="card-text">
+                        Contagiados: ${ casesPerOneMillion.toLocaleString() } <br>
+                        Fallecidos: ${ deathsPerOneMillion.toLocaleString() } <br>
+                        Testeos: ${ testsPerOneMillion.toLocaleString() }
+                  </p>
                  </div>
                 </div>
                 `;
 
         });
 
+let fecha = new Date();
 
-// let fecha = new Date();
+// ayer
+fecha.setDate(fecha.getDate() - 1);
 
-// let mes = fecha.getMonth()+1; //obteniendo mes
-// let dia = fecha.getDate(); //obteniendo dia
-// let ano = fecha.getFullYear(); //obteniendo añ
+let mes = fecha.getMonth()+1 < 10
+          ?  "0".concat(fecha.getMonth()+1)
+          : fecha.getMonth()+1 ; //obteniendo mes
 
-// console.log('Fecha' , { mes , dia , ano });
-                
-// let dias = 5;
-// let urlCovid     = `https://disease.sh/v3/covid-19/historical/ARG?lastdays=${ dias }`;
-        
-// Total de casos acumulado por dia 
-// fetch( urlCovid )
-//         .then( resp => resp.json() )
-//         .then( resp => {
+let dia = fecha.getDate(); //obteniendo dia
+let ano = fecha.getFullYear(); //obteniendo a
 
-//                 console.log( resp );
-//         } );
+fecha_completa = dia + "/" + mes + "/" + ano;
+fecha_completa_ayer = "27" + "/" + mes + "/" + ano;
+let totalER = 0;
+
+console.log({ fecha_completa });
+Papa.parse('/data.csv', {
+	download: true,
+	step: function(row) {
+
+                totalER += row.data[4] == "Entre Ríos"
+                           ? Number(row.data[7])
+                           : 0;
+
+                if ( row.data[4] == "Entre Ríos" && row.data[0] == fecha_completa )
+                {
+                       [fecha,dia_inicio,dia_cuarentena_dnu260,
+                        osm_admin_level_2,osm_admin_level_4,
+                        osm_admin_level_8,tot_casosconf,
+                        nue_casosconf_diff,tot_fallecidos,
+                        nue_fallecidos_diff,tot_recuperados,
+                        tot_terapia,test_RT_PCR_negativos,
+                        test_RT_PCR_total,transmision_tipo,
+                        informe_tipo,informe_link,
+                        observacion,covid19argentina_admin_level_4] = row.data;
+
+                        divTableroProvinciaHoy.innerHTML =
+                        `
+                         <div class="card-body carta_ultrep">
+                          <h5 class="card-title"> ${ fecha }</h5>
+                          <p class="card-text">
+                                Contagiados: ${ nue_casosconf_diff } <br>
+                                Fallecidos: ${ nue_fallecidos_diff } <br>
+                                Test. Acum.: ${ totalER } <br>
+                          </p>
+                         </div>
+                        `;
+                } else if ( row.data[4] == "Entre Ríos" && row.data[0] == fecha_completa_ayer )
+                {
+                       [fecha,dia_inicio,dia_cuarentena_dnu260,
+                        osm_admin_level_2,osm_admin_level_4,
+                        osm_admin_level_8,tot_casosconf,
+                        nue_casosconf_diff,tot_fallecidos,
+                        nue_fallecidos_diff,tot_recuperados,
+                        tot_terapia,test_RT_PCR_negativos,
+                        test_RT_PCR_total,transmision_tipo,
+                        informe_tipo,informe_link,
+                        observacion,covid19argentina_admin_level_4] = row.data;
+
+                        divTableroProvinciaAyer.innerHTML =
+                        `
+                         <div class="card-body carta_ultrep">
+                          <h5 class="card-title"> ${ fecha }</h5>
+                          <p class="card-text">
+                                Contagiados: ${ nue_casosconf_diff } <br>
+                                Fallecidos: ${ nue_fallecidos_diff } <br>
+                          </p>
+                         </div>
+                        `;
+                }
 
 
-// var results = Papa.parse('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv');
-// console.log(results);
+
+	},
+	complete: function() {}
+});
 
 
-// Papa.parse('/data.csv', {
-// 	download: true,
-// 	step: function(row) {
-// 		console.log("Row:", row.data);
-// 	},
-// 	complete: function() {
-// 		console.log("All done!");
-// 	}
-// });
-
-// console.log();
